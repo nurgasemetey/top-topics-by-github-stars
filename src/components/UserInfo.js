a1693cd4c0eb4cca7f6fd316bf63050e6cfc3587
Cristian Dominguez <cristiand391@protonmail.com>
2020-01-29 11:17:23 -0300

    UserInfo: show more accurate date

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,21 +3,28 @@ import Repos from "./Repos";
 import User from "./User";
 
 export default function UserInfo({ user }) {
+  const createdYear = user.created_at.getFullYear();
+  const actualYear = new Date().getFullYear();
+  const moreThanOneYear = actualYear - createdYear > 1;
+
+  const joined_at = moreThanOneYear
+    ? `${actualYear - createdYear} years`
+    : "less than one year";
+
   return (
     <section className="section">
       <div className="container is-fluid">
         <div className="columns is-centered">
           <div className="column is-three-quarters">
-            <h1 className="title is-3 has-text-centered">
+            <h1 className="title is-2 has-text-centered">
               Overview for{" "}
               <span className="has-text-primary">{user.login}</span>
             </h1>
-            <p className="has-text-centered">
-              Joined GitHub{" "}
-              <strong>
-                {new Date().getFullYear() - user.created_at.getFullYear()} years
-                ago
-              </strong>{" "}
+            <p className="has-text-centered is-size-6">
+              Joined GitHub <strong>{joined_at} ago</strong>{" "}
+              <time className="is-size-6" dateTime={user.created_at}>
+                ({user.created_at.toDateString()})
+              </time>
             </p>
 
             <User user={user} />
